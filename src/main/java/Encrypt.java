c3f0b3fc0e9a4a4e897e8c0208fe5685f29e0e0b
MarkMwago2 <markmwago@gmail.com>
2019-06-15 13:15:47 +0300

    all encrypt tests passed

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -1,2 +1,19 @@
 public class Encrypt {
+    private String mName;
+    private int mKey;
+
+    public int getmKey() {
+        return mKey;
+    }
+
+    public String getmName() {
+        return "a";
+    }
+
+    public Encrypt(String name, int key){
+        mName = name;
+        mKey = key;
+    }
+
+
 }
--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -1,2 +1,11 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class EncryptTest {
+
+    @Test
+    public void Encrypt_acceptsaString() {
+        Encrypt testEncrypt = new Encrypt("a", 2);
+        assertEquals("a", testEncrypt.getmName());
+    }
 }
