20ec222e96c97b25f5ccd2b248a716498e21dd91
MarkMwago2 <markmwago@gmail.com>
2019-06-15 13:40:55 +0300

    all decrypt tests are passing

--- src/main/java/Decrypt.java
+++ src/main/java/Decrypt.java
@@ -1,2 +1,29 @@
 public class Decrypt {
+    private String mName;
+    private int mKey;
+
+    public int getmKey() {
+        return mKey;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public Decrypt(String name, int key){
+        mName = name;
+        mKey = key;
+    }
+    public String Decrypt(){
+        String result = "";
+        for (int i= 0; i<mName.length(); i++) {
+            char ch = mName.charAt(i);
+            char shifted = (char) (ch - mKey);
+            result += shifted;
+        }
+        return result;
+    }
 }
+
+
+
--- src/test/java/DecryptTest.java
+++ src/test/java/DecryptTest.java
@@ -1,5 +1,25 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DecryptTest {
 
+    @Test
+    public void Decrypt_acceptsaString() {
+        Decrypt testDecrypt = new Decrypt("a", 2);
+        assertEquals("a", testDecrypt.getmName());
+
+    }
+
+    @Test
+    public void Decrypt_acceptsaKey() {
+        Decrypt testDecrypt = new Decrypt("a", 2);
+        assertEquals(2, testDecrypt.getmKey());
+    }
+
+    @Test
+    public void Decrypt_decryptsbyKey() {
+        Decrypt testDecrypt = new Decrypt("ecv", 2);
+        assertEquals("cat", testDecrypt.Decrypt());
+    }
 }
